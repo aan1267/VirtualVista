69850189553496100184c70b91669e92bf1c1b79
aach1267 <aanchalsharmax1@gmail.com>
2024-11-10 02:33:01 +0530

    added changes

--- client/src/Components/Message.jsx
+++ client/src/Components/Message.jsx
@@ -10,19 +10,11 @@ function Message() {
     const socket=useSocket()
     const { username,room } =useUserContext()
 
-    // const handleMessage=(msg)=>{
-    //   console.log("Received message:", msg)
-    //     setMessages((prevMessages)=>{
-    //       const updatedMessages = [...prevMessages, msg];
-    //     console.log("Updated messages:", updatedMessages); // Log updated messages
-    //     return updatedMessages;
-    //   })
-    // }
     useEffect(()=>{
       const handleMessage = (msg) => {
-        console.log("Received message:", msg); // Debugging line
-        setMessages((prevMessages) => [...prevMessages, msg]);
-    };
+        console.log("Received message:", msg); 
+        setMessages((prevMessages) => [...prevMessages, msg])
+    }
 
         socket.on("chat-message",handleMessage)
         return ()=>{
@@ -36,7 +28,7 @@ function Message() {
       (e) => {
           e.preventDefault();
           if(input){
-            socket.emit("chat-message", { username:username, message:input ,room})
+            socket.emit("chat-message", { username, message:input ,room})
             setInput("")
           }
       },
@@ -46,16 +38,19 @@ function Message() {
   return (
     <div className="chattingRoom">
       <h1>Chat</h1>
-      {messages.map((msg, index) => (
+      <p className={messages.length === 0 ? "nochat":""}></p>
+       <div className="messages">
+       {messages.map((msg, index) => (
                     <p key={index}>
                         {msg.username}<br/>{msg.message}
                     </p>
                 ))}
-      <form className="chattingbox" onSubmit={handleSubmitForm}>
-        <input  placeholder="enter your chat" type="text" id="chattingArea" onChange={(e)=>setInput(e.target.value)} value={input} />
-        <Button type="submit" variant="contained" >
-          Send
-        </Button>
+       </div>
+      <form onSubmit={handleSubmitForm}>
+        <div className="chattingbox">
+        <input  placeholder="enter your chat" type="text"  onChange={(e) => setInput(e.target.value)} value={input} />
+        <Button type="submit" variant="contained">Send</Button>
+        </div>
       </form>
     </div>
   )
