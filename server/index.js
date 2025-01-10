92652e36d803d2ce81ab37dcc7769b53288c3c83
aach1267 <aanchalsharmax1@gmail.com>
2025-01-10 14:46:14 +0530

    add changes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const {Server}=require("socket.io")
 const http=require("http")
 const cors=require("cors")
 
-
+const port= process.env.PORT || 3000
 const allowedOrigin = process.env.NODE_ENV === 'production' 
    ? [process.env.FRONTEND_URL_PROD]
   : [process.env.FRONTEND_URL_DEV]
@@ -73,7 +73,7 @@ socket.on("peernegodone",({to,ans})=>{
 
 })
 
-httpServer.listen( process.env.PORT || 3000,'0.0.0.0',()=>{
+httpServer.listen(port,'0.0.0.0',()=>{
     console.log(`Server is running on port ${port}`)
 })
 
