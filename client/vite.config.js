3ae90cbd1d1c479f43126c8c37922ea5059d6c34
aach1267 <aanchalsharmax1@gmail.com>
2025-01-10 16:44:43 +0530

    add changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port:4173 || 5173,
+    port:4173,
     host: '0.0.0.0',
   },
  plugins: [react()],
