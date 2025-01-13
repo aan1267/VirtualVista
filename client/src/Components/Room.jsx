0833e943f301957566d29c75ea4419c0acb965b8
aach1267 <aanchalsharmax1@gmail.com>
2025-01-13 23:31:33 +0530

    add

--- client/src/Components/Room.jsx
+++ client/src/Components/Room.jsx
@@ -145,7 +145,8 @@ function Room() {
       window.location.href="/lobby"
     },5000)
     toast.error("Opponent has disconnected. The call has ended.", {
-      position: "top-center",       
+      position: "top-center", 
+      autoClose:5000,      
       hideProgressBar: true, 
       closeOnClick: true,  
       pauseOnHover: true,  
